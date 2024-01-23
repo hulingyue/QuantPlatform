db7a77497d9c63459ccc9a5be362656c716e506f
hulingyue <511809125@qq.com>
2024-01-23 17:37:15 +0800

    [add] HeaderView

---
+++ frontend/src/components/HeaderView.vue
@@ -0,0 +1,46 @@
+ <template>
+    <el-menu 
+        :default-active="activeIndex"
+        class="header-menu"
+        mode="horizontal"
+        :ellipsis="false"
+        @select="handleSelect">
+
+        <el-menu-item index="0">
+            <img style="width: 100%;" src="../assert/logo.jpg" alt="logo">
+        </el-menu-item>
+        <el-sub-menu index="1">
+            <template #title>品类</template>
+            <el-menu-item index="1-1">数字货币</el-menu-item>
+            <el-menu-item index="1-1">期货</el-menu-item>
+            <el-menu-item index="1-1">股票</el-menu-item>
+            <el-menu-item index="1-1">现货</el-menu-item>
+        </el-sub-menu>
+        <div class="flex-grow"></div>
+
+        <div>
+            <el-avater>未登录</el-avater>
+        </div>
+    </el-menu>
+ </template>
+
+ <script setup lang="ts">
+import { ref } from 'vue'
+
+const activeIndex = ref('1')
+
+const handleSelect = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
+}
+</script>
+
+<style scoped>
+.flex-grow {
+    flex-grow: 1;
+}
+
+.el-menu {
+    width: 100%;
+    height: 60px;
+}
+</style>
--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import State_404 from './components/State_404.vue'
+import HomePage from './components/HomePage.vue'
 import Helloworld from './components/HelloWorld.vue'
 
 
 const routes = [
       { path: '/', component: Helloworld }
+    , { path: '/home', component: HomePage }
     , { path: '/:pathMatch(.*)', component: State_404 }
 ];
 
