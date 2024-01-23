49c70ff19cad8fcfb98c9cb3f5d24843f038c273
hulingyue <511809125@qq.com>
2024-01-23 16:44:44 +0800

    [add] 404 page

---
+++ frontend/src/components/State_404.vue
@@ -0,0 +1,13 @@
+<template>
+    <el-card>
+        <p>抱歉，页面未找到。</p>
+        <router-link to="/">返回首页</router-link>
+    </el-card>
+</template>
+  
+<script lang="ts">
+export default {
+    name: 'NotFound'
+}
+</script>
+  
--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-
+import State_404 from './components/State_404.vue'
 import Helloworld from './components/HelloWorld.vue'
 
 
 const routes = [
-    {path: '/', component: Helloworld}
+      { path: '/', component: Helloworld }
+    , { path: '/:pathMatch(.*)', component: State_404 }
 ];
 
 const router = createRouter({
