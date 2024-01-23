3d4e7fca85188d357e942faf89e90c82fba821ac
hulingyue <511809125@qq.com>
2024-01-23 17:37:59 +0800

    [add] HomePage

---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+    <el-header><HeaderView></HeaderView></el-header>
+    <el-main></el-main>
+    <el-footer><FooterView></FooterView></el-footer>
+</template>
+
+<script setup lang="ts">
+import HeaderView from './HeaderView.vue'
+import FooterView from './FooterView.vue'
+</script>
