fa196f97c30a09ee5c7f4b5ded335cebcade2178
hulingyue <511809125@qq.com>
2024-01-23 20:07:50 +0800

    [rm] remove HelloWorld.vue

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -2,12 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import State_404 from './components/State_404.vue'
 import HomePage from './components/HomePage.vue'
-import Helloworld from './components/HelloWorld.vue'
 
 
 const routes = [
-      { path: '/', component: Helloworld }
-    , { path: '/home', component: HomePage }
+      { path: '/', component: HomePage }
     , { path: '/:pathMatch(.*)', component: State_404 }
 ];
 
