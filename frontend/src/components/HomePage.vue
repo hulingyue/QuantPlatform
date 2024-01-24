716cc5efd3ca2cfa1013f15dbbf15fb6c98016c7
hulingyue <511809125@qq.com>
2024-01-24 09:13:09 +0800

    [add] echarts demo

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,12 +1,13 @@
 <template>
     <el-header><HeaderView></HeaderView></el-header>
-    <el-main></el-main>
+    <el-main><StockView></StockView></el-main>
     <el-footer><FooterView></FooterView></el-footer>
 </template>
 
 <script setup lang="ts">
 import HeaderView from './HeaderView.vue'
 import FooterView from './FooterView.vue'
+import StockView from './StockView.vue'
 
 document.title = "ARTrade量化交易平台"
 </script>
