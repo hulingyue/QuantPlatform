b0d63c12d170741bdaeafcc3f97a787376940429
hulingyue <511809125@qq.com>
2024-01-24 17:39:33 +0800

    [update] show stockView

--- frontend/src/components/page/HomePage.vue
+++ frontend/src/components/page/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
     <el-header><HeaderView></HeaderView></el-header>
     <el-main>
-        <!-- <StockView></StockView> -->
+        <StockView></StockView>
     </el-main>
     <el-footer><FooterView></FooterView></el-footer>
 </template>
@@ -9,7 +9,7 @@
 <script setup lang="ts">
 import HeaderView from '../part/HeaderView.vue'
 import FooterView from '../part/FooterView.vue'
-// import StockView from '../part/StockView.vue'
+import StockView from '../part/StockView.vue'
 
 document.title = "ARTrade量化交易平台"
 </script>
