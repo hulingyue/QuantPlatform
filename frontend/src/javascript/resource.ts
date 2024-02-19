cc4a93bfb0a2d1c8a1ea74ab97698fbe3d1d54ae
hulingyue <511809125@qq.com>
2024-02-19 14:44:35 +0800

    [update] auto set the host of ws

--- frontend/src/javascript/resource.ts
+++ frontend/src/javascript/resource.ts
@@ -1,14 +1,19 @@
+function extractDomainFromUrl(url: string): string {
+    const domainMatch = url.match(/(?:https?:\/\/)?(?:www\.)?([^:\/\s]+)/);
+    return domainMatch ? domainMatch[1] : '';
+}
+
 export function createResourceConfig(testMode: boolean = false) {
     if (testMode === true) {
         return {
-            BybitMarket: "ws://localhost:5678/"
+            BybitMarket: "ws://" + extractDomainFromUrl(window.location.host) + ":5678/"
         }
     } else {
         return {
-            BybitMarket: "ws://localhost:5678/"
+            BybitMarket: "ws://" + extractDomainFromUrl(window.location.host) + ":5678/"
         }
     }
 }
 
 // export const ResourceConfig = createResourceConfig(false);
-export const ResourceConfig = createResourceConfig(true);
+export const ResourceConfig = createResourceConfig(false);
